423478a8069c194c8fb2f856773dc02b6a4d2de2
fakhriram233 <fakhriramadhan233@outlook.com>
2022-06-17 13:15:56 +0700

    Day 2 Chapter 2 Task, Routing Done, Post with console log done, some styling left

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const app = express()
 
 const port = 8000
 
+app.set('view engine', 'hbs'); //set view engine
+
+app.use('/assets', express.static(__dirname + '/assets'))
+
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', function(request,response){
+    response.render('index')
+})
+
+app.get('/contact-me', function (request,response){
+    response.render('contact-me')
+})
+
+app.get('/add-project', function (request,response){
+    response.render('add-project')
+})
+
+app.get('/project-detail', function (request,response){
+    response.render('project-detail')
+})
+
+app.post('/add-project', function(request,response) {
+    console.log(request.body);
+
+})
 
 app.listen(port, function() {
     console.log(`Server running on port ${port}`);
